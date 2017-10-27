b5510e150e35c38e7a5ba74c7c48aba6a5309d80
lyskos97 <lyskos97@gmail.com>
2017-10-27 22:00:45 +0600

    chore: change port #2

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import express from 'express';
 import expressGraphQL from 'express-graphql';
 import schema from './schema/Schema';
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(
-  '/',
+  '/graphql',
   expressGraphQL(req => ({
     schema,
     graphiql: true,
